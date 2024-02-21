cc8acc8cb0168cf4f1295c74bdc315e46a4a9b75
gyooooh <155943403+gyooooh@users.noreply.github.com>
2024-02-21 18:48:41 +0900

    commit

--- src/main/java/spring/mvc/basic_1team/service/OrderService.java
+++ src/main/java/spring/mvc/basic_1team/service/OrderService.java
@@ -6,22 +6,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.ui.Model;
+
 public interface OrderService {
 
 	// 선택 상품 주문결제 페이지
-	public void selectPdOrderBefore(HttpServletRequest req, HttpServletResponse res)
+	public void selectPdOrderBefore(HttpServletRequest req, Model model)
 			throws ServletException, IOException;
 	
 	// 선택 결제 처리
-	public void selectPdOrderPayment(HttpServletRequest req, HttpServletResponse res)
+	public void selectPdOrderPayment(HttpServletRequest req, Model model)
 			throws ServletException, IOException;
 	
 	// 전체 상품 주문결제 페이지
-	public void allPdOrderBefore(HttpServletRequest req, HttpServletResponse res)
+	public void allPdOrderBefore(HttpServletRequest req, Model model)
 			throws ServletException, IOException;
 
 	// 전체 결제 처리
-	public void allPdOrderPayment(HttpServletRequest req, HttpServletResponse res)
+	public void allPdOrderPayment(HttpServletRequest req, Model model)
 	throws ServletException, IOException;
 
 }
--- src/main/java/spring/mvc/basic_1team/service/OrderServiceImpl.java
+++ src/main/java/spring/mvc/basic_1team/service/OrderServiceImpl.java
@@ -7,80 +7,89 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import spring.mvc.basic_1team.dao.OrderDAO;
 import spring.mvc.basic_1team.dao.OrderDAOImpl;
 import spring.mvc.basic_1team.dto.CustomerDTO;
 import spring.mvc.basic_1team.dto.OrderDTO;
 
+@Service
 public class OrderServiceImpl implements OrderService{
 
-	// 선택 상품 주문결제 페이지
+	@Autowired
+	private OrderDAO dao;
+	
+	// 선택 상품 주문결제 페이지 (조회)
 	@Override
-	public void selectPdOrderBefore(HttpServletRequest req, HttpServletResponse res)
+	public void selectPdOrderBefore(HttpServletRequest req, Model model)
 			throws ServletException, IOException {
 		System.out.println("<< 서비스 - selectPdOrderBefore(req, res) >>");
 		
 		String user_id = (String)req.getSession().getAttribute("sessionID");
 		
-		OrderDAOImpl dao = OrderDAOImpl.getInstance();
+		//OrderDAOImpl dao = OrderDAOImpl.getInstance();
 		
 		List<OrderDTO> list = dao.selectPdList(user_id);
 		int selectpdPrice = dao.orderTotalPrice1(user_id);
 		CustomerDTO c_dto = dao.userInfo(user_id);
 		
-		req.setAttribute("list", list);
-		req.setAttribute("selectpdPrice", selectpdPrice);
-		req.setAttribute("c_dto", c_dto);
+		model.addAttribute("list", list);
+		model.addAttribute("selectpdPrice", selectpdPrice);
+		model.addAttribute("c_dto", c_dto);
 		
 	}
 	
-	// 선택 결제 처리 
+	// 선택 결제 처리  (수정)
 	@Override
-	public void selectPdOrderPayment(HttpServletRequest req, HttpServletResponse res)
+	public void selectPdOrderPayment(HttpServletRequest req, Model model)
 			throws ServletException, IOException {
 		System.out.println("<< 서비스 - selectPdOrderPayment(req, res) >>");
 		
 		String user_id = (String)req.getSession().getAttribute("sessionID");
 		
-		OrderDAOImpl dao = OrderDAOImpl.getInstance();
+		//OrderDAOImpl dao = OrderDAOImpl.getInstance();
 		
 		int updateCnt = dao.isPaymentUpdate1(user_id);
 	
-		req.setAttribute("updateCnt", updateCnt);
+		model.addAttribute("updateCnt", updateCnt);
 		
 	}
 
-	// 전체 상품 주문결제 페이지
+	// 전체 상품 주문결제 페이지 (전체조회)
 	@Override
-	public void allPdOrderBefore(HttpServletRequest req, HttpServletResponse res) 
+	public void allPdOrderBefore(HttpServletRequest req, Model model)
 			throws ServletException, IOException {
 		System.out.println("<< 서비스 - allPdOrderBefore(req, res) >>");
 		
 		String user_id = (String)req.getSession().getAttribute("sessionID");
 		
-		OrderDAOImpl dao = OrderDAOImpl.getInstance();
+		//OrderDAOImpl dao = OrderDAOImpl.getInstance();
 		
 		List<OrderDTO> list = dao.allPdList(user_id);
 		int selectpdPrice = dao.orderTotalPrice2(user_id);
 		CustomerDTO c_dto = dao.userInfo(user_id);
 		
-		req.setAttribute("list", list);
-		req.setAttribute("selectpdPrice", selectpdPrice);
-		req.setAttribute("c_dto", c_dto);
+		model.addAttribute("list", list);
+		model.addAttribute("selectpdPrice", selectpdPrice);
+		model.addAttribute("c_dto", c_dto);
 	}	
 	
-	// 전체 결제 처리 
+	// 전체 결제 처리 (수정)
 	@Override
-	public void allPdOrderPayment(HttpServletRequest req, HttpServletResponse res)
+	public void allPdOrderPayment(HttpServletRequest req, Model model)
 			throws ServletException, IOException {
 		System.out.println("<< 서비스 - allPdOrderPayment(req, res) >>");
 		
 		String user_id = (String)req.getSession().getAttribute("sessionID");
 		
-		OrderDAOImpl dao = OrderDAOImpl.getInstance();
+		//OrderDAOImpl dao = OrderDAOImpl.getInstance();
 		
 		int updateCnt = dao.isPaymentUpdate2(user_id);
 		
-		req.setAttribute("updateCnt", updateCnt);
+		model.addAttribute("updateCnt", updateCnt);
 	
 	}
 }
